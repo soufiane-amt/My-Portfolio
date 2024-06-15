bf070c0fedfe70a1ed7d4f120b18216f58319882
soufiane-amt <soufianeamt11@gmail.com>
2024-06-15 13:13:24 +0100

    The NavBar is ready but some styling improvments have to take place

--- src/app/components/GeneralScrollSpy.tsx
+++ src/app/components/GeneralScrollSpy.tsx
@@ -1,28 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import profilePic from '@public/NavBar/logo.png';
 import Image from 'next/image';
+import  style from '../styles/GeneralScrollSpy.module.css';
 
 
 export default function GeneralScrollSpy ()
 {
     return (
-        <nav className="navbar navbar-expand-sm bg_general_color navbar-dark fixed-top">
+        <nav className="navbar navbar-expand-sm bg_general_color navbar-dark fixed-top d-flex justify-content-around py-5">
             <div>
-                <Image src={profilePic} alt="Logo"/>
+                <Image src={profilePic} alt="Logo" width={155} height={85}/>
             </div>
  
             <div>
-                <ul className="navbar-nav">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="#portfolio">Portfolio</a></li>
-                    <li><a href="#awards">Awards</a></li>
+                <ul className="navbar-nav  d-flex justify-content-between ">
+                    <li className="nav-item h2 px-4 ">
+                        <a className="nav-link text-white" href="#home">Home</a> {/* Apply Bootstrap utility class */}
+                    </li>
+                    <li className="nav-item h2 px-4 ">
+                        <a className="nav-link text-white" href="#about">About</a>
+                    </li>
+                    <li className="nav-item h2 px-4 ">
+                        <a className="nav-link text-white" href="#services">Services</a>
+                    </li>
+                    <li className="nav-item h2 px-4 ">
+                        <a className="nav-link text-white" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li className="nav-item h2 px-4 ">
+                        <a className="nav-link text-white" href="#awards">Awards</a>
+                    </li>
                 </ul>
             </div>
 
             <div>
-                <button className="btn border custom_btn_border_color">
+                <button className={`btn  ${style.custom_btn_style} `}>
                     Appointment
                 </button>
             </div>
