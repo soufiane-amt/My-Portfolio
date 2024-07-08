a162385d544425a48d9e0a3e9f6c40abb9120bf2
soufiane-amt <soufianeamt11@gmail.com>
2024-07-08 14:32:27 +0100

    Contacts animation works

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -5,6 +5,7 @@ import { LegacyRef, RefObject, useEffect } from 'react';
 import useInView from '../CostumHooks/UseInView';
 import SectionTitle from './SectionTitle';
 import FadeTop from './FadeTop';
+import useInViewOnce from '../CostumHooks/UseInViewOnce';
 
 
 
@@ -55,7 +56,7 @@ function GithubContactItem ()
 function Messager ()
 {
     return (
-        <div className={`container ${style.messager} d-flex flex-column justify-content-center py-3 mt-5`}>
+        <div className={`container ${style.messager} ${style.fadeInRight} d-flex flex-column justify-content-center py-3 mt-5`}>
             <div className='d-md-flex justify-content-md-between '>
                 <div  className='d-flex justify-content-center my-3 mx-3 flex-fill'>
                     <input type='text' placeholder='Full Name' className='form-control border rounded-0' required>
@@ -86,6 +87,7 @@ interface contactProps {
 }
 const Contact: React.FC<contactProps> = ({ setCurrentPage, reference }) => {
     const  isInView  = useInView(reference);
+    const  isInViewOnce  = useInViewOnce(reference);
 
     useEffect (()=>{
         if (isInView)
@@ -93,16 +95,21 @@ const Contact: React.FC<contactProps> = ({ setCurrentPage, reference }) => {
     }, [isInView])
 
     return (
-        <FadeTop reference={reference}>
             <SectionTitle sectionName='Contacts' reference={reference}>
-                <div className={`d-flex flex-column flex-md-row justify-content-between align-items-lg-center  ${style.contacts_board} pt-4 ps-4 ps-ms-0`}>
-                        <EmailContactItem/>
-                        <LinkedInContactItem/>
-                        <GithubContactItem/>
-                    </div>
-                <Messager/>
+                {
+                    isInViewOnce && (
+                        <>
+                        <div className={`d-flex flex-column flex-md-row justify-content-between align-items-lg-center  ${style.contacts_board} ${style.fadeInLeft} pt-4 ps-4 ps-ms-0`}>
+                                <EmailContactItem/>
+                                <LinkedInContactItem/>
+                                <GithubContactItem/>
+                            </div>
+                        <Messager/>
+                        </>
+
+                    )
+                }
             </SectionTitle>
-        </FadeTop>
     )
 }
 
