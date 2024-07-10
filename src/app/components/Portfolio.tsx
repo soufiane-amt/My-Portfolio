ddbd7f55c4236cdba4bcb1a9b5d8b022faf7fa47
soufiane-amt <soufianeamt11@gmail.com>
2024-07-10 13:56:01 +0100

    Each Component is now linked to github reference

--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -12,94 +12,109 @@ import useInViewOnce from '../CostumHooks/UseInViewOnce';
 
 const projectList = [
     {
-        name : "ft_transcendance",
+        name : "Ft_transcendance",
         type : "Web",
         decription : "Online multiplayers Ping Pong game",
-        imgSrc : "/ProjectElement/ft_transcendance.png"
+        imgSrc : "/ProjectElement/ft_transcendance.png",
+        url : "https://github.com/soufiane-amt/ft_transcendance"
     },
     {
         name : "Webserv",
         type : "Systems Programming",
         decription : "Nginix inspired web server project",
-        imgSrc : "/ProjectElement/webserv.webp"
+        imgSrc : "/ProjectElement/webserv.webp",
+        url : "https://github.com/soufiane-amt/webserv"
     },
     {
         name : "Inception",
-        type : "Containerization",
+        type : "Systems Programming",
         decription : "Containerization project",
-        imgSrc : "/ProjectElement/inception.png"
+        imgSrc : "/ProjectElement/inception.png",
+        url : "https://github.com/soufiane-amt/inception"
     },
     {
         name : "Minishell",
         type : "Systems Programming",
         decription : "Reimplimentation of a shell interpreter",
-        imgSrc : "/ProjectElement/minishell.png"
+        imgSrc : "/ProjectElement/minishell.png",
+        url : "https://github.com/soufiane-amt/minishell"
     },
     {
         name : "ft_containers",
         type : "Systems Programming",
         decription : "C++ Data structures low level programing ",
-        imgSrc : "/ProjectElement/ft_containers.webp"
+        imgSrc : "/ProjectElement/ft_containers.webp",
+        url : "https://github.com/soufiane-amt/ft_containers"
     },
     {
         name : "Cub3D",
         type : "Graphics",
         decription : "Raycasting game project",
-        imgSrc : "/ProjectElement/cubTd.webp"
+        imgSrc : "/ProjectElement/cubTd.webp",
+        url : "https://github.com/soufiane-amt/cub3d"
     },
     {
         name : "Libft",
-        type : "Systems Programming",
+        type : "Others",
         decription : "Reimplementation of C functions in a custom library",
-        imgSrc : "/ProjectElement/libft.png"
+        imgSrc : "/ProjectElement/libft.png",
+        url : "https://github.com/soufiane-amt/Libft"
     },
     {
-        name : "born2beroot",
-        type : "Others",
+        name : "Born2beroot",
+        type : "Systems Programming",
         decription : "C++ Data structures low level programing ",
-        imgSrc : "/ProjectElement/borntoberoot.png"
+        imgSrc : "/ProjectElement/borntoberoot.png",
+        url : "https://github.com/soufiane-amt/"
     },
     {
         name : "Netpractice",
         type : "Others",
         decription : "Raycasting game project",
-        imgSrc : "/ProjectElement/netpractice.png"
+        imgSrc : "/ProjectElement/netpractice.png",
+        url : "https://github.com/soufiane-amt/NetPractice"
     },
     {
-        name : "philosophers",
-        type : "Systems Programming",
+        name : "Philosophers",
+        type : "Others",
         decription : "Reimplementation of C functions in a custom library",
-        imgSrc : "/ProjectElement/philosophers.webp"
+        imgSrc : "/ProjectElement/philosophers.webp",
+        url : "https://github.com/soufiane-amt/Philosopher"
     },
     {
-        name : "pipex",
-        type : "Systems Programming",
+        name : "Pipex",
+        type : "Others",
         decription : "C++ Data structures low level programing ",
-        imgSrc : "/ProjectElement/pipex.webp"
+        imgSrc : "/ProjectElement/pipex.webp",
+        url : "https://github.com/soufiane-amt/pipex"
     },
     {
-        name : "get_next_line",
-        type : "Systems Programming",
-        decription : "Raycasting game project",
-        imgSrc : "/ProjectElement/get_next_line.webp"
-    },
-    {
-        name : "so_long",
-        type : "Graphics",
-        decription : "Raycasting game project",
-        imgSrc : "/ProjectElement/so_long.webp"
-    },
-    {
-        name : "push_swap",
-        type : "Graphics",
-        decription : "Raycasting game project",
-        imgSrc : "/ProjectElement/push_swap.webp"
-    },
-    {
-        name : "ft_printf",
+        name : "Get_next_line",
         type : "Others",
         decription : "Raycasting game project",
-        imgSrc : "/ProjectElement/ft_printf.webp"
+        imgSrc : "/ProjectElement/get_next_line.webp",
+        url : "https://github.com/soufiane-amt/get_next_line"
+    },
+    {
+        name : "So_long",
+        type : "Graphics",
+        decription : "Raycasting game project",
+        imgSrc : "/ProjectElement/so_long.webp",
+        url : "https://github.com/soufiane-amt/so_long"
+    },
+    {
+        name : "Push_swap",
+        type : "Others",
+        decription : "Raycasting game project",
+        imgSrc : "/ProjectElement/push_swap.webp",
+        url : "https://github.com/soufiane-amt/push_swap"
+    },
+    {
+        name : "Ft_printf",
+        type : "Others",
+        decription : "Raycasting game project",
+        imgSrc : "/ProjectElement/ft_printf.webp",
+        url : "https://github.com/soufiane-amt/ft_printf"
     },
 ]
 
@@ -109,34 +124,33 @@ interface ProjectCardType {
     name : string, 
     decription : string, 
     imgSrc : string, 
+    url : string, 
 }
-const ProjectCard: React.FC<{ project: ProjectCardType; index: number }> = ({ project, index }) =>
-{
+const ProjectCard: React.FC<{ project: ProjectCardType; index: number }> = ({ project, index }) => {
     const reference = useRef(null);
+    const isInView = useInViewOnce(reference);
 
-    const isInView = useInViewOnce(reference)
     return (
-        <div ref={reference} style={{ animationDelay: `${index * 0.5}s` }} className={`${style.project_card} ${style.fade_in} col-lg-4 col-md-6  position-relative`}>
-        { (isInView && 
-            <div style={{ animationDelay: `${index * 0.1}s` }} className={`${style.fade_in} ${style.fade_in_up} `}>
-                                <div>
-                                    <img 
-                                        src={`${project.imgSrc}`} 
-                                        alt="Project Image" 
-                                        className='img-fluid'
-                                        />
-                                </div>
-                                <div className={` position-absolute ${style.card_description} d-flex flex-column `}>
-                                    <h4 className='f_famil_raleway fw_18_px fw-bold'>{`${project.name}`} </h4>
-                                    <p className='f_style_italic f_famil_roboto fw_14_px'>{`${project.decription}`} </p>
-                                </div>
-            </div>
-            )
-
-            }
-            </div>
-    )
+        <div ref={reference} style={{ animationDelay: `${index * 0.01}s` }} className={`${style.project_card} ${style.fade_in} col-lg-4 col-md-6 position-relative`}>
+            {isInView && (
+                <a href={project.url} style={{ animationDelay: `${index * 0.1}s` }} className={`${style.fade_in} ${style.fade_in_up} ${style.project_link}`}>
+                    <div>
+                        <img 
+                            src={project.imgSrc} 
+                            alt="Project Image" 
+                            className='img-fluid'
+                        />
+                    </div>
+                    <div className={`position-absolute ${style.card_description} d-flex flex-column`}>
+                        <h4 className='f_famil_raleway fw_18_px fw-bold'>{project.name}</h4>
+                        <p className='f_style_italic f_famil_roboto fw_14_px'>{project.decription}</p>
+                    </div>
+                </a>
+            )}
+        </div>
+    );
 }
+
 
 
 
